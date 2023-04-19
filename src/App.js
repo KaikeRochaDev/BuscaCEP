e7a37e7656122c1f519977a38baf66a18019308b
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-19 18:19:34 -0300

    Getting Started with JavaScript Logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import {useState} from 'react'
 import {FiSearch} from 'react-icons/fi'
 import './styles.css'
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    alert('teste')
+  }
+
   return (
     <div className="container">
       <h1 className="title">Busca CEP</h1>
 
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu CEP..."/>
+        <input type="text" placeholder="Digite seu CEP..." value={input} onChange={(e) => setInput(e.target.value)}/>
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color='#fff'/>
         </button>
       </div>
