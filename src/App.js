69a04c4009f90b13ee1fbb9ed644c560ed482162
Kaike Rocha <dev.kaikerocha@gmail.com>
2023-04-19 18:12:50 -0300

    Finalizing the css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import {FiSearch} from 'react-icons/fi'
+import './styles.css'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste projeto</h1>
+    <div className="container">
+      <h1 className="title">Busca CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu CEP..."/>
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color='#fff'/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 709022422</h2>
+
+        <span>
+          Rua Teste
+        </span>
+
+        <span>
+          Complemento: Algum
+        </span>
+
+        <span>
+          Bairro
+        </span>
+
+        <span>
+          Seropédica - RJ
+        </span>
+      </main>
     </div>
   );
 }
